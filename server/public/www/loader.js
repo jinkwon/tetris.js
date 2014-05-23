a6d6791ac44625bc4bfadbaaa6348e427ecac824
LeeJinKwon <lee.jinkwon@nhn.com>
2014-05-23 10:22:33 +0900

    update uglify plugin

---
+++ server/public/www/loader.js
@@ -0,0 +1,4 @@
+/*! Tetris - v0.1.0 - 2014-05-23
+* https://github.com/Jinkwon/tetris.js
+* Copyright (c) 2014 LeeJinKwon; Licensed MIT */
+requirejs.config({baseUrl:"./",urlArgs:"",waitSeconds:45,paths:{underscore:"vendor/underscore/underscore",backbone:"vendor/backbone/backbone",jquery:"vendor/jquery/dist/jquery.min","jquery-ui":"lib/jquery-ui",iscroll:"vendor/iscroll/build/iscroll","socket.io":"vendor/socket.io-client/dist/socket.io.min",glmatrix:"lib/glmatrix",howler:"vendor/howler/howler.min",tetris:"Tetris.min"},shim:{backbone:{deps:["underscore","jquery"],exports:"Backbone"},jquery:{exports:"$"},"jquery-ui":{deps:["jquery"]},tetris:{deps:["backbone","jquery","jquery-ui","glmatrix","socket.io","iscroll","howler"]}}}),requirejs(["tetris"],function(){app.tetris.init("production")});
